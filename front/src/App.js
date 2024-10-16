58aff122d4e2feeb2adb372439a3cd330ad53397
bhyeon99 <woobh1210@gmail.com>
2024-10-16 09:41:56 +0900

    데이터베이스 연결 성공

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,25 @@
 import { useEffect, useState } from 'react';
+import UserList from './components/UserList';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     // NestJS 서버(3001번 포트)에 있는 API 호출
-    fetch('http://localhost:3001/api/example')
-      .then((response) => response.json())
+    fetch('http://localhost:3001/users') // API 경로 확인
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then((data) => setData(data))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
 
   return (
     <div>
-      <h1>Data from NestJS API:</h1>
-      {data ? <pre>{JSON.stringify(data, null, 2)}</pre> : <p>Loading...</p>}
+      <UserList users={data} /> {/* UserList에 users prop 전달 */}
     </div>
   );
 }
