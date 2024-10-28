501df7e8077878fe1c778c1ccc229a778122cb17
baeg0pa <shk0630wkd@gmail.com>
2024-10-28 15:17:47 +0900

    옥션카우즈 모듈 추가

--- back/src/auction-cows/auction-cows.module.ts
+++ back/src/auction-cows/auction-cows.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuctionCowsService } from './auction-cows.service';
 import { AuctionCowsController } from './auction-cows.controller';
 import { AuctionCow } from './auction-cow.entity';
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AuctionCow])],
+  imports: [TypeOrmModule.forFeature([AuctionCow, User])],
   controllers: [AuctionCowsController],
   providers: [AuctionCowsService],
 })
